cf216fc196804067be553e2e1b8161a3ad3fdda5
Hayasshida <pedrohayashida9@gmail.com>
2024-05-27 23:46:03 -0300

    feat: tensor

--- src/screens/Feed/index.jsx
+++ src/screens/Feed/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Button } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from './styles';
 import { View, Image, KeyboardAvoidingView, FlatList } from 'react-native';
-import { Text, TextInput } from 'react-native-paper';
+import { Text, TextInput, Button } from 'react-native-paper';
 import * as Dimensions from "../../constants/dimensions";
 import userIcon from "../../../assets/user-icon.jpg";
 import { useState } from 'react';
 import sad from "../../../assets/sad.png"
 import Post from '../../components/Post';
-import { toxicityTest } from '../../IA/methods/toxicity';
+import { toxicityTest } from '../../IA/methods/toxicity'; 
+
 
 export default function Feed({ navigation }) {
 
@@ -21,30 +21,30 @@ export default function Feed({ navigation }) {
 
   const [ contentPost, setContentPost ] = useState("");
 
-  const tryPublish = async (posts) => {
-    const response = await toxicityTest(posts);
+  const tryPublish = async (post) => {
+    const response = await toxicityTest(post);
 
-    if(response[0] === undefined){
+    if(response == []){
       setPostInfo(postInfo[postInfo.length] = {
         icon: userIcon,
         ownerPost: "@bagre",
-        post: posts
+        post: post
       })
+      return;
     }
-
+    
     console.log(response);
   };
 
     return(
-      
         <SafeAreaView style={styles.container}>
-          {postInfo[0] === undefined ? 
+          {postInfo[0].post === null ? 
             <View style={styles.nothing}>
               <Image source={sad} style={{width: 64, height: 64, marginBottom: 12 }}/>
-              <Text style={{color: "white", fontSize: 22,}}children="Nothing here yet"/>
+              <Text style={styles.text}children="Nothing here yet"/>
             </View>
           :
-            <View style={styles.renderFlat}>
+            <View style={{height: Dimensions.height*0.7}}>
               <FlatList
                 data={postInfo}
                 renderItem={({item}) => <Post icon={item.icon} ownerPost={item.ownerPost} post={item.post}/>}
@@ -55,15 +55,15 @@ export default function Feed({ navigation }) {
           <KeyboardAvoidingView behavior="position" enabled>
               
             <View style={styles.makePost}>
-              <View style={{flexDirection: 'row', alignSelf: "flex-start", alignItems: "center", marginLeft: Dimensions.width*0.06, gap: 12}}>
-                <Image source={userIcon} style={{height: 50, width: 50, borderRadius: 50, margin: 0}}/>
-                <Text children="@bagre" style={{color: "white", fontSize: 22}}/>
+              <View style={styles.post}>
+                <Image source={userIcon} style={styles.icon}/>
+                <Text children="@bagre" style={styles.text}/>
               </View>
 
               <TextInput style={{backgroundColor: "transparent", width: Dimensions.width*0.8, fontSize: 22}} placeholder="Make your post" value={contentPost} onChangeText={setContentPost} textColor="white"/>
 
-              <Button onPress={() => tryPublish(contentPost)} mode="contained" buttonColor="#99272d" textColor="white" style={{width: Dimensions.width*0.6, alignItems: "center", justifyContent: "center", padding: 0, margin: 0}}>
-                <Text style={{color: "white", fontSize: 20}}>Publish</Text>  
+              <Button onPress={() => tryPublish(contentPost)} mode="contained" buttonColor="#99272d" textColor="white" style={styles.button}>
+                <Text style={styles.text}>Publish</Text>  
               </Button>
             </View>
 
--- src/screens/Feed/styles.js
+++ src/screens/Feed/styles.js
@@ -18,15 +18,36 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 48,
         borderTopEndRadius: 48,
     },
-    renderFlat: {
-        height: Dimensions.height*0.7,
-    },
     nothing: {
         height: Dimensions.height*0.7,
         justifyContent: "center",
         alignItems: "center",
         
-    }
+    },
+    text: {
+        color: "white",
+        fontSize: 21,
+    },
+    button: {
+        width: Dimensions.width*0.6, 
+        alignItems: "center", 
+        justifyContent: "center", 
+        padding: 0, 
+        margin: 0
+    },
+    icon: {
+        height: 50, 
+        width: 50, 
+        borderRadius: 50, 
+        margin: 0
+    },
+    post: {
+        flexDirection: 'row', 
+        alignSelf: "flex-start", 
+        alignItems: "center", 
+        marginLeft: Dimensions.width*0.06, 
+        gap: 12
+    },
 
 });
 
